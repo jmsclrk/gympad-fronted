02f627bbb2069521fbc568d5546db450e1d7e355
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2021-03-05 13:18:26 +0000

    Add login route for unauthnticated page visits

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import { User } from './models/user'
+
+
+export default function App() {
+  const [user, setUser] = useState<User | null>(null)
+
   return (
     <div className="App">
       <Switch>
-        <Route path="/" exact children={<div><h2>Home Page</h2></div>} />
+        {user ? (
+            <Route path="/" exact children={<div ><h2>Home Page</h2></div>} />
+        ) : (
+          <>
+            <Route path="/" exact children={<div><h2>Login Page</h2></div>} />
+            <Route path="/" render={() => <Redirect to="/" />} />
+          </>
+        )}
       </Switch>
     </div>
   );
 }
-
-export default App;
